3a5c3642b8b11ac2635ef3270ddf29c56d22b070
Creditbox <61037811+creditbox123@users.noreply.github.com>
2020-03-25 16:15:28 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://developers.livechatinc.com/docs",
+    siteUrl: "https://github.com/angelchat/angelchat.developers",
     title: "LiveChat Platform Docs & API Reference",
     description:
       "The LiveChat Platform offers much more than just a messaging tool. There's a number of possibilities that help you grow a business, which brings customer satisfaction.",
